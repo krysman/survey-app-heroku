be007cb47557de97b774b52e2d5e8ca54b4f9ac1
krysman <saprykin8sept@gmail.com>
2015-03-12 18:10:11 +0300

    initial commit

---
+++ src/main/java/com/saprykin/surveyapp/App.java
@@ -0,0 +1,98 @@
+package com.saprykin.surveyapp;
+
+import static spark.Spark.get;
+import static spark.SparkBase.setPort;
+
+/**
+ * Created by Ivan on 3/12/2015.
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+
+        //Heroku assigns different port each time, hence reading it from process.
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if(process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        setPort(port);
+
+        //DaoTest daoTest = new DaoTest();
+        //final String dbTestString = daoTest.testDb();
+
+        get("/hello", (request, response) -> "<html><head><h1>Hello World!</h1></head><body>" + "<h2>" /*+ dbTestString*/ + "</h2>" + "</body></html>");
+    }
+
+/*
+
+
+    private  Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    public String testDb() {
+        String result = "testing...<br>";
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            result += "<br>successfully get connection...";
+        } catch(URISyntaxException | SQLException e) {
+            result += "<br>couldn't get connection!";
+        }
+
+        try {
+            Statement stmt;
+            if(connection != null) {
+                stmt = connection.createStatement();
+                result += "<br>successfully created statement...";
+            }
+            else {
+                stmt = null;
+                result += "<br>connection is null, and statement couldn't be created";
+            }
+            StringBuilder res = new StringBuilder();
+
+            if(stmt != null) {
+                stmt.executeUpdate("DROP TABLE IF EXISTS users");
+                stmt.executeUpdate(
+                        "CREATE TABLE users " +
+                        "(id SERIAL NOT NULL PRIMARY KEY ," +
+                        " email VARCHAR(100) NOT NULL )"
+                );
+
+                stmt.executeUpdate("INSERT INTO users (email) VALUES ('foo@bar.com'), ('bar@foo.com'), ('foobar@bf.com') ");
+                ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+                res.append("Reading from DB:<br>");
+                while(rs.next()) {
+                    res.append("id: ");
+                    res.append(rs.getInt("id"));
+                    res.append(" e-mail: ");
+                    res.append(rs.getString("email"));
+                    res.append("<br>");
+                }
+            }
+
+            result = res.toString();
+
+        } catch(SQLException e) {
+            result += "<br>";
+            result += e.toString();
+        }
+
+        return result;
+    }
+ */
+
+}
