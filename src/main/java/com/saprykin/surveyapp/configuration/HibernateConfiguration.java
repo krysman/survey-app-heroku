e1cb00260f40f2a09df51738ce1c02395b2d50a7
krysman <saprykin8sept@gmail.com>
2015-03-17 00:57:23 +0300

    fix wrong jdbc driver

--- src/main/java/com/saprykin/surveyapp/configuration/HibernateConfiguration.java
+++ src/main/java/com/saprykin/surveyapp/configuration/HibernateConfiguration.java
@@ -64,19 +64,19 @@ public class HibernateConfiguration {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        /*properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
         properties.put("hibernate.hbm2ddl.auto", "create");
         properties.put("hibernate.connection.characterEncoding", "UTF-8");
-        properties.put("hibernate.connection.useUnicode", "true");*/
+        properties.put("hibernate.connection.useUnicode", "true");
 
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        /*properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto")); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         properties.put("hibernate.connection.characterEncoding", environment.getRequiredProperty("hibernate.connection.characterEncoding"));
-        properties.put("hibernate.connection.useUnicode", environment.getRequiredProperty("hibernate.connection.useUnicode"));
+        properties.put("hibernate.connection.useUnicode", environment.getRequiredProperty("hibernate.connection.useUnicode"));*/
 
         return properties;
     }
