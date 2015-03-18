be4743a8bf27278a90cdbeb18ed7da97524f9994
krysman <saprykin8sept@gmail.com>
2015-03-18 15:10:21 +0300

    add bootstrap template html

--- src/main/java/com/saprykin/surveyapp/App.java
+++ src/main/java/com/saprykin/surveyapp/App.java
@@ -29,7 +29,7 @@ public class App {
 //        staticFileLocation("/classes/public/");
 //        staticFileLocation("classes/public");
 //        staticFileLocation("\\classes\\public");
-        staticFileLocation("classes\\public");
+//        staticFileLocation("classes\\public");
 
         setPortForApp();
         setUpLog4jProperties();
